b497298c27ca722f8e44a7134ff8ee5dd43bd74b
Martí Bosch <marti.bosch@protonmail.com>
2025-01-16 20:16:33 +0100

    feat: use geopandas for plots

--- swiss_urban_trees/plot.py
+++ swiss_urban_trees/plot.py
@@ -2,23 +2,26 @@
 
 from os import path
 
+import geopandas as gpd
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import rasterio as rio
 import seaborn as sns
-from deepforest import visualize
+from rasterio import plot
 
 from swiss_urban_trees import utils
 
 
 def plot_annot_vs_pred(
-    annot_df: pd.DataFrame,
-    pred_df: pd.DataFrame,
+    annot_gdf: gpd.GeoDataFrame,
+    pred_gdf: gpd.GeoDataFrame,
     tile_dir: utils.PathDType,
     *,
     figwidth: float | None = None,
     figheight: float | None = None,
     pred_title: str = "predictions",
+    legend: bool = True,
     plot_annot_kwargs: utils.KwargsDType = None,
     plot_pred_kwargs: utils.KwargsDType = None,
 ):
@@ -26,34 +29,50 @@ def plot_annot_vs_pred(
 
     Parameters
     ----------
-    annot_df, pred_df : pd.DataFrame
-        Annotations and predictions data frames respectively.
+    annot_gdf, pred_gdf : gpd.GeoDataFrame
+        Annotations and predictions geo-data frames respectively.
     tile_dir : path-like
         Path to the directory containing the tiles.
     figwidth, figheight : numeric, optional
         Figure width and height. If None, the matplotlib defaults are used.
-    pred_title : str, optional
+    pred_title : str, default "predictions"
         Title for the predictions plots.
+    legend : bool, default True
+        Whether to show the legend on the last plot.
     plot_annot_kwargs, plot_pred_kwargs : mapping, optional
-        Keyword arguments to pass to `deepforest.visualize.plot_predictions` when
-        plotting annotations and predictions, respectively.
+        Keyword arguments to pass to `geopandas.GeoDataFrame.plot` when plotting
+        annotations and predictions respectively.
 
     Returns
     -------
     fig : matplotlib.figure.Figure
 
     """
-    img_filenames = annot_df["image_path"].unique()
-    patch_sizes = pred_df["patch_size"].unique()
+
+    # small helper to plot image and gdf
+    def _plot_img_and_gdf(src, gdf, ax, **plot_kwargs):
+        plot.show(src, with_bounds=False, ax=ax)
+        left, right = ax.get_xlim()
+        bottom, top = ax.get_ylim()
+        gdf.assign(**{"geometry": gdf.boundary}).plot(ax=ax, **plot_kwargs)
+        ax.set_xlim(left, right)
+        ax.set_ylim(bottom, top)
+
+    img_filenames = annot_gdf["image_path"].unique()
+    patch_sizes = pred_gdf["patch_size"].unique()
 
     if figwidth is None:
         figwidth = plt.rcParams["figure.figsize"][0]
     if figheight is None:
         figheight = plt.rcParams["figure.figsize"][1]
     if plot_annot_kwargs is None:
-        plot_annot_kwargs = {}
+        _plot_annot_kwargs = {}
+    else:
+        _plot_annot_kwargs = plot_annot_kwargs.copy()
     if plot_pred_kwargs is None:
-        plot_pred_kwargs = {}
+        _plot_pred_kwargs = {}
+    else:
+        _plot_pred_kwargs = plot_pred_kwargs.copy()
 
     num_rows = len(img_filenames)
     num_cols = len(patch_sizes) + 1
@@ -62,35 +81,57 @@ def plot_annot_vs_pred(
     )
     # in case there is only one image
     if num_rows == 1:
-        axes = [axes]
+        axes = np.array([axes])
+
+    if legend:
+        _plot_annot_kwargs["legend"] = _plot_annot_kwargs.pop("legend", False)
+        _plot_pred_kwargs["legend"] = _plot_pred_kwargs.pop("legend", True)
+        _plot_pred_kwargs["legend_kwds"] = _plot_annot_kwargs.pop(
+            "legend_kwds", {"loc": "center right", "bbox_to_anchor": (1.5, 0.5)}
+        )
 
     for img_filename, ax_row in zip(img_filenames, axes):
         with rio.open(
             path.join(tile_dir, f"{path.splitext(img_filename)[0]}.tif")
         ) as src:
-            img_arr = src.read()
-            ax_row[0].imshow(
-                visualize.plot_predictions(
-                    img_arr,
-                    annot_df[annot_df["image_path"] == img_filename],
-                    **plot_annot_kwargs,
-                )[:, :, ::-1]
+            _plot_img_and_gdf(
+                src,
+                annot_gdf[annot_gdf["image_path"] == img_filename],
+                ax_row[0],
+                **_plot_annot_kwargs,
             )
             # ACHTUNG: `pred_df` was assigned the `.tif` image instead of the jpeg
             # TODO: how to handle this properly?
-            for (patch_size, patch_df), ax in zip(
-                pred_df[
-                    pred_df["image_path"] == f"{path.splitext(img_filename)[0]}.tif"
+            for (patch_size, patch_gdf), ax in zip(
+                pred_gdf[
+                    pred_gdf["image_path"] == f"{path.splitext(img_filename)[0]}.tif"
                 ].groupby("patch_size"),
                 ax_row[1:],
             ):
-                ax.imshow(
-                    visualize.plot_predictions(
-                        img_arr,
-                        patch_df,
-                        **plot_pred_kwargs,
-                    )[:, :, ::-1]
-                )
+                _plot_img_and_gdf(src, patch_gdf, ax, **_plot_pred_kwargs)
+
+    if legend:
+        # ideally we could do something like
+        # fig.legend(
+        #     *fig.axes[-1].get_legend_handles_labels(),
+        #     loc="center right",
+        #     bbox_to_anchor=(1, 1),
+        # )
+        # but we get no handles and labels for the legend, maybe related to
+        # https://github.com/geopandas/geopandas/issues/660
+        row_indices = np.arange(len(axes))
+        # TODO: how to handle even number of rows?
+        middle_row = row_indices[len(row_indices) // 2]
+        # iterate first half of rows
+        for row_i in row_indices:
+            # the first axis of each row is the annotations, so no need to remove
+            if row_i == middle_row:
+                # in the middle row, leave the legend of the last axis
+                _axes = axes[row_i][1:-1]
+            else:
+                _axes = axes[row_i][1:]
+            for ax in _axes:
+                ax.get_legend().remove()
 
     for title, ax in zip(
         ["annotations"]
